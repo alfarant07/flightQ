2c3b869d6df5683cb11f180081ef3ca1c5556b4d
Anthony Alfaro <alfaro2005@yahoo.com>
2026-08-17 15:52:33 -0500

    Feat:
    Add Navbar component to handle flight search input UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { SiTheplanetarysociety } from "react-icons/si";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav class="py-4 flex bg-gray-200 items-center justify-between">
+      <div class="flex items-center gap-2">
+        {" "}
+        <SiTheplanetarysociety className="text-2xl"/>{" "}
+      <h1 className="text-lg">FlightQ</h1>
+</div>
 
-export default Navbar
+      <div class = "flex gap-1.5 ">
+        <form className=" flex gap-1.5">
+          <input className="w-full border" type="text" placeholder="Origin" />
+          <input className="w-full border" type="text" placeholder="Destination" />
+        </form>
+        <button className="border p-2 " type="search"> Search</button>
+      </div>
+
+      <div className="w-24" aria-hidden="true" />
+
+    </nav>
+  );
+};
+
+export default Navbar;
