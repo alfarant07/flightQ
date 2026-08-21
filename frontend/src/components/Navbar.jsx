6214d9c28a624aac327313e95562bfb85ea2c180
Anthony Alfaro <alfaro2005@yahoo.com>
2026-08-20 22:00:46 -0500

    feat: add departure date and cabin class inputs to form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use, useState } from "react";
 import { SiTheplanetarysociety } from "react-icons/si";
 
 const Navbar = () => {
@@ -6,20 +6,46 @@ const Navbar = () => {
     <nav className="py-4 flex bg-gray-200 items-center justify-between">
       <div className="flex items-center gap-2">
         {" "}
-        <SiTheplanetarysociety className="text-2xl"/>{" "}
-      <h1 className="text-lg">FlightQ</h1>
-</div>
+        <SiTheplanetarysociety className="text-2xl" />{" "}
+        <h1 className="text-lg">FlightQ</h1>
+      </div>
 
-      <div className = "flex gap-1.5 ">
+      <div className="flex gap-1.5 ">
         <form className=" flex gap-1.5">
-          <input className="w-full border text-center rounded-sm" type="text" placeholder="Origin" />
-          <input className="w-full border text-center rounded-sm" type="text" placeholder="Destination" />
+          <input
+            className="w-full border text-center rounded-sm"
+            type="text"
+            placeholder="Origin"
+            name="Origin"
+          />
+          <input
+            className="w-full border text-center rounded-sm"
+            type="text"
+            placeholder="Destination"
+            name="destination"
+          />
+          <input
+            type="date"
+            name="departureDate"
+            className="w-full border text-center rounded-sm"
+          />
+          <select
+            className="w-full border text-center rounded-sm"
+            name="cabinClass"
+          >
+            <option value="economy">Economy</option>
+            <option value="premium_economy">Premium Economy</option>
+            <option value="business">Business</option>
+            <option value="first">First</option>
+          </select>
         </form>
-        <button className="border p-2 hover:bg-gray-300 " type="submit"> Search</button>
+        <button className="border p-2 hover:bg-gray-300 " type="submit">
+          {" "}
+          Search
+        </button>
       </div>
 
       <div className="w-24" aria-hidden="true" />
-
     </nav>
   );
 };
