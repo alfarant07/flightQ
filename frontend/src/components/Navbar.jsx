985d616290ff8174a5597e8fdc769ea7c19696d8
Anthony Alfaro <alfaro2005@yahoo.com>
2026-08-24 20:58:48 -0500

    refactor: refactor Navbar to give it single responsibility
    Refactored navbar to follow single responsibility principle
    delegated navbar search logic to Searchform component which
    is rendered inside navbar.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,7 @@
-import React, { use, useState } from "react";
 import { SiTheplanetarysociety } from "react-icons/si";
+import Searchform from "./Searchform";
 
-const Navbar = (e) => {
-
-
-  const [state, setState] = useState({
-    origin: "",
-    destination: "",
-    departureDate: "",
-    cabinClass: ""
-  })
-
-  const handleChange = (e) =>{
-    setState(prev => ({ ...prev, [e.target.name]: e.target.value }));
-  }
-
-
-
-
+const Navbar = () => {
   return (
     <nav className="py-4 flex bg-gray-200 items-center justify-between">
       <div className="flex items-center gap-2">
@@ -27,48 +11,7 @@ const Navbar = (e) => {
       </div>
 
       <div className="flex gap-1.5 ">
-        <form className=" flex gap-1.5">
-          <input
-            className="w-full border text-center rounded-sm"
-            type="text"
-            placeholder="Origin"
-            name="origin"
-            value ={state.origin}
-            onChange={handleChange}
-
-          />
-          <input
-            className="w-full border text-center rounded-sm"
-            type="text"
-            placeholder="Destination"
-            name="destination"
-            value = {state.destination}
-            onChange={handleChange}
-
-          />
-          <input
-            type="date"
-            name="departureDate"
-            className="w-full border text-center rounded-sm"
-            value = {state.departureDate}
-            onChange={handleChange}
-          />
-          <select
-            className="w-full border text-center rounded-sm"
-            name="cabinClass"
-            value = {state.cabinClass}
-            onChange={handleChange}
-          >
-            <option value="economy">Economy</option>
-            <option value="premium_economy">Premium Economy</option>
-            <option value="business">Business</option>
-            <option value="first">First</option>
-          </select>
-        </form>
-        <button className="border p-2 hover:bg-gray-300 " type="submit">
-          {" "}
-          Search
-        </button>
+        <Searchform />
       </div>
 
       <div className="w-24" aria-hidden="true" />
