9c0b2df708f912df461047c48132e9114a3dfe5a
Anthony Alfaro <alfaro2005@yahoo.com>
2026-08-26 22:37:49 -0500

    refactor: decoupled Navbar and Searchform
    Decouple Navbar and Searchform as a future card component requires
    Searchform state thus splitting component to allow card and Search
    to share state from a common ancestor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,18 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar'
+import Searchform from './components/Searchform'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <section className='grid grid-cols-3 items-center bg-gray-200 py-4'>
     <Navbar/>
+    <Searchform className="flex justify-center"/>
+    <div></div>
+    </section>
       
     </>
   )
