7131224734eeedeeeecd9234535318b8291c7323
Anthony Alfaro <alfaro2005@yahoo.com>
2026-08-27 20:39:38 -0500

    Chore: remove redundant import/variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar'
 import Searchform from './components/Searchform'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <section className='grid grid-cols-3 items-center bg-gray-200 py-4'>
