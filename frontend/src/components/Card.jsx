499e1ff1db6464cc91728e24e422efc33495ca65
Anthony Alfaro <alfaro2005@yahoo.com>
2026-09-08 23:51:10 -0500

    Style: Add additional flight data to Card
    Added Additional flight info regarding airline name and departing-
    -arrival airports

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -9,16 +9,23 @@ const Card = ({ offer }) => {
           <section className="h-10 w-10">
             <img src={offer.owner.logo_symbol_url}></img>
           </section>
-          <h1 className="p-2">
-            {offer.slices[0].origin.city_name} to{" "}
-            {offer.slices[0].destination.city_name}
-          </h1>
+          <section className="flex-row">
+            <h1 className="p-1">
+              {offer.slices[0].origin.city_name} to{" "}
+              {offer.slices[0].destination.city_name}
+            </h1>
+            <h2 className="text-gray-500 text-s ml-2">{offer.owner.name}</h2>
+          </section>
         </div>
-        <section className="flex text-gray-500 ml-4">
-          <h2>{getFlightTime(offer.slices[0].segments[0].departing_at)}</h2>
-          <h2>-</h2>
-          <h2>{getFlightTime(offer.slices[0].segments[0].arriving_at)}</h2>
-        </section>
+          <section className="flex text-gray-500 ml-4 mt-1">
+          <h2 className="mr-2">{offer.slices[0].origin.iata_code}</h2>
+
+            <h2>{getFlightTime(offer.slices[0].segments[0].departing_at)}</h2>
+            <h2>-</h2>
+            <h2>{getFlightTime(offer.slices[0].segments[0].arriving_at)}</h2>
+            <h2 className="ml-2">{offer.slices[0].destination.iata_code}</h2>
+
+          </section>
 
         <h2 className="text-m p-2">
           {parseDuration(offer.slices[0].duration)}
